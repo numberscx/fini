3549845f45ea377963a7735257ffffef7a7e3db9
numberscx <s.c.x.1@163.com>
2023-04-23 22:48:54 +0800

    nit

---
+++ fini/src/main/java/com/dodgeking/fini/FiniApplication.java
@@ -0,0 +1,13 @@
+package com.dodgeking.fini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiniApplication.class, args);
+	}
+
+}
